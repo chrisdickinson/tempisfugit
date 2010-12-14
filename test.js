93d09ec0f28352f12bda611aac49ef8730684926
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-12-14 00:54:37 -0600

    We can now reack IDXv2 and packed refs -- and OFS deltas.

--- test.js
+++ test.js
@@ -5,13 +5,28 @@ var tempis = require('tempis'),
     path = require('path'),
     util = require('util');
 
-i = Date.now();
+var usage = process.memoryUsage();
+
+var start = Date.now(),
+    i = 0,
+    data = [],
+    last;
+
 Repo.open(process.cwd(), function(err, repo) {
-  repo.head.lookup(function(err, commit) {
-    console.log(commit.message);
-    console.log(Date.now() - i);
-
-
-
-  });
+  var log = function(err, commit) {
+    ++i;
+    if(commit && commit.parentOIDs && commit.parentOIDs[0]) {
+      if(commit.oid.toHex() === commit.parentOIDs[0].toHex()) {
+        console.log('saw '+commit.oid.toHex()+' twice');
+        console.log(i + ' commits in ' + (Date.now()-start) + 'ms');
+      } else {
+        repo.lookup(commit.parentOIDs[0], arguments.callee);
+        last = commit;
+      }
+    } else {
+      console.log(i + ' commits in ' + (Date.now()-start) + 'ms');
+      console.log('last oid: '+last.parentOIDs[0].toHex());
+    }
+  };
+  repo.head.lookup(log);
 });
